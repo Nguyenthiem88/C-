f156d48459404478e2481c98ae3101e6a61174a2
Nguyenthiem88 <92871733+Nguyenthiem88@users.noreply.github.com>
2023-01-10 18:27:27 +0700

    upcode

--- ConsoleApp/session 4/CallApi.cs
+++ ConsoleApp/session 4/CallApi.cs
@@ -8,17 +8,6 @@ namespace ConsoleApp.session_4
 {
     internal class CallApi
     {
-        public async Task<Product> FetchData()
-        {
-            string url = "https://dummyjson.com/products/1";
-
-            HttpClient client= new HttpClient();
-            var rs = await client.GetAsync(url);
-            if(rs.StatusCode==HttpStatusCode.OK)
-            {
-                string responseText = await rs.Content.ReadAsStringAsync();
-                Product p = Jon
-            }
-        }
+        
     }
 }
---
+++ house.cm/IHouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace house.cm
+{
+    internal interface IHouse
+    {
+        public void input();
+        public void display();
+    }
+
+    internal class House
+    {
+
+    }
+}
